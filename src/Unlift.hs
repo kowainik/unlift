855f7ef49a247a651c21bc2cd15a011bcd13b32b
Veronika Romashkina <vrom911@gmail.com>
2021-04-21 04:56:03 +0100

    Create the project

---
+++ src/Unlift.hs
@@ -0,0 +1,15 @@
+{- |
+Copyright: (c) 2021 Kowainik
+SPDX-License-Identifier: MPL-2.0
+Maintainer: Kowainik <xrom.xkov@gmail.com>
+
+Typeclass for monads that can be unlifted to arbitrary base monads
+-}
+
+module Unlift
+    ( projectName
+    ) where
+
+
+projectName :: String
+projectName = "unlift"
